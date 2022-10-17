c93dc6f6f82499d866ee795c766e1eac746f62f3
least-mouse <ethanladwig@gmail.com>
2022-10-17 17:20:46 -0500

    migration from neocities

---
+++ load.js
@@ -0,0 +1,5 @@
+$(function(){
+      $("#header").load("https://least-mouse.neocities.org/header.html"); 
+      $("#navigation").load("https://least-mouse.neocities.org/navigation.html"); 
+      $("#footer").load("https://least-mouse.neocities.org/footer.html");
+});
